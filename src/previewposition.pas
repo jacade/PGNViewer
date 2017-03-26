60721352c4a7f7b8e3fa10a1201013a305b9e123
jacade <j-c.dette@arcor.de>
2017-03-26 11:18:49 +0200

    Added a preview position and an ancestor class to supress warnings about abstract methods

--- src/previewposition.pas
+++ src/previewposition.pas
@@ -21,42 +21,119 @@ unit previewposition;
 interface
 
 uses
-  Classes, SysUtils, Position, Pieces;
+  Classes, SysUtils, Position, Pieces, MoveList;
 
 type
 
   { TPreviewPosition }
 
   TPreviewPosition = class(TPosition)
+  public
+    function GetAllLegalMoves: TMoveList; override;
+    function MoveToStr(AMove: TMove; MoveToStrOptions: TMoveToStrOptions): string;
+      override;
+    procedure PlayMove(AMove: TMove); override;
+    procedure SetupInitialPosition; override;
+    function ValidateMove(AMove: TMove): boolean; override;
+  end;
+
+  { TPreviewPosition2x2 }
+
+  TPreviewPosition2x2 = class(TPreviewPosition)
   protected
     function GetCountOfFiles: byte; override;
     function GetCountOfRanks: byte; override;
     function GetSquares(Index: integer): TPieceType; override;
-  public
+  end;
 
+  { TPreviewPosition2x6 }
+
+  TPreviewPosition2x6 = class(TPreviewPosition)
+  protected
+    function GetCountOfFiles: byte; override;
+    function GetCountOfRanks: byte; override;
+    function GetSquares(Index: integer): TPieceType; override;
   end;
 
 implementation
 
 { TPreviewPosition }
 
-function TPreviewPosition.GetCountOfFiles: byte;
+function TPreviewPosition.GetAllLegalMoves: TMoveList;
+begin
+  Result := nil;
+end;
+
+function TPreviewPosition.MoveToStr(AMove: TMove;
+  MoveToStrOptions: TMoveToStrOptions): string;
+begin
+  Result := '';
+end;
+
+procedure TPreviewPosition.PlayMove(AMove: TMove);
+begin
+
+end;
+
+procedure TPreviewPosition.SetupInitialPosition;
+begin
+
+end;
+
+function TPreviewPosition.ValidateMove(AMove: TMove): boolean;
+begin
+  Result := False;
+end;
+
+{ TPreviewPosition2x2 }
+
+function TPreviewPosition2x2.GetCountOfFiles: byte;
 begin
   Result := 2;
 end;
 
-function TPreviewPosition.GetCountOfRanks: byte;
+function TPreviewPosition2x2.GetCountOfRanks: byte;
 begin
   Result := 2;
 end;
 
-function TPreviewPosition.GetSquares(Index: integer): TPieceType;
+function TPreviewPosition2x2.GetSquares(Index: integer): TPieceType;
 begin
   case Index of
     0: Result := ptBQueen;
     1: Result := ptBKing;
     2: Result := ptWQueen;
     3: Result := ptWKing;
+  end;
+end;
+
+{ TPreviewPosition2x6 }
+
+function TPreviewPosition2x6.GetCountOfFiles: byte;
+begin
+  Result := 6;
+end;
+
+function TPreviewPosition2x6.GetCountOfRanks: byte;
+begin
+  Result := 2;
+end;
+
+function TPreviewPosition2x6.GetSquares(Index: integer): TPieceType;
+begin
+  case Index of
+    0: Result := ptBRook;
+    1: Result := ptBKnight;
+    2: Result := ptBBishop;
+    3: Result := ptBQueen;
+    4: Result := ptBKing;
+    5: Result := ptBPawn;
+    6: Result := ptWRook;
+    7: Result := ptWKnight;
+    8: Result := ptWBishop;
+    9: Result := ptWQueen;
+    10: Result := ptWKing;
+    11: Result := ptWPawn;
   end;
 end;
 
