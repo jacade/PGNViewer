f1b8a566d6e312dbd3fa67cb29849c90e82754a6
jacade <j-c.dette@arcor.de>
2017-03-27 11:15:02 +0200

    Add ability to play moves and display notation

--- src/mainform.pas
+++ src/mainform.pas
@@ -21,9 +21,10 @@ unit mainform;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
-  ComCtrls, Menus, ActnList, StdActns, Board, NotationMemo, EngineView, PGNGame,
-  Position, Database, PGNdbase, MoveList, AboutForm, SettingsForm;
+  Classes, SysUtils, gvector, FileUtil, Forms, Controls, Graphics, Dialogs,
+  ExtCtrls, ComCtrls, Menus, ActnList, StdActns, Board, NotationMemo,
+  EngineView, PGNGame, Position, Database, PGNdbase, MoveList, AboutForm,
+  SettingsForm, Game;
 
 type
 
@@ -53,9 +54,13 @@ type
     procedure FormDestroy(Sender: TObject);
     procedure miAboutClick(Sender: TObject);
     procedure miSettingsClick(Sender: TObject);
+    procedure NotationMemo1ClickMove(Sender: TObject; AMove: TMove);
+    procedure NotationMemo1Enter(Sender: TObject);
+    procedure NotationMemo1SelectionChange(Sender: TObject);
   private
     Databases: TDatabaseList;
     BaseIndex, GameIndex: integer;
+    CurrentGame: TGame;
   public
     { public declarations }
   end;
@@ -76,22 +81,93 @@ begin
   Databases := TDatabaseList.Create;
   Databases.Add(TPGNDatabase.Create);
   Databases.Items[0].Add(TPGNGame.Create);
-  with Databases.Items[0].Items[0] do
-  begin
-    InitialPosition.SetupInitialPosition;
-    CurrentPosition.SetupInitialPosition;
-  end;
+  CurrentGame := Databases.Items[0].Items[0];
+  CurrentGame.InitialPosition.SetupInitialPosition;
+  CurrentGame.CurrentPosition.SetupInitialPosition;
   Board1.PieceDirectory := '../Pieces';
   Board1.CurrentPosition := TStandardPosition.Create;
   Board1.CurrentPosition.Copy(Databases.Items[0].Items[0].CurrentPosition);
+  with NotationMemo1.AddLineStyle^ do
+  begin
+    CommentaryStyle.Color := clGreen;
+    CommentaryStyle.Style := [];
+    MoveStyle.Color := clBlack;
+    MoveStyle.Style := [fsBold];
+    NAGStyle.Color := clRed;
+    NAGStyle.Style := [];
+    NumberStyle.Color := clBlack;
+    NumberStyle.Style := [fsBold];
+    CommentaryNewLine := True;
+    CommentaryIndent := 25;
+    NeedsNewLine := False;
+    LineIndent := 0;
+  end;
+  with NotationMemo1.AddLineStyle^ do
+  begin
+    CommentaryStyle.Color := clGreen;
+    CommentaryStyle.Style := [];
+    MoveStyle.Color := clBlue;
+    MoveStyle.Style := [];
+    NAGStyle.Color := clBlue;
+    NAGStyle.Style := [];
+    NumberStyle.Color := clBlue;
+    NumberStyle.Style := [];
+    CommentaryNewLine := False;
+    NeedsNewLine := True;
+    LineIndent := 25;
+  end;
+  with NotationMemo1.MoveToStrOptions do
+  begin
+    CaptureSymbol := csx;
+    PieceLetters := PieceLetters_DE;
+    PromotionSymbol := psNone;
+    ShowEnPassantSuffix := False;
+    ShowPawnLetter := False;
+  end;
 end;
 
 procedure TForm1.Board1MovePlayed(AMove: TMove);
+var
+  i: integer;
 begin
   if Databases.Items[BaseIndex].Items[GameIndex].CurrentPosition.ValidateMove(AMove) then
   begin
-
-  end;
+    if CurrentGame.CurrentPlyNode.Children.Size = 0 then
+    begin
+      // New move at the end entered, play it
+      CurrentGame.AddMove(AMove);
+    end
+    else
+    begin
+      for i := 0 to CurrentGame.CurrentPlyNode.Children.Size - 1 do
+      begin
+        // the new move is the same as one old one, so we can play it
+        if (AMove = CurrentGame.CurrentPlyNode.Children[i].Data.Move) then
+        begin
+          CurrentGame.GoOneMoveForward(i);
+          Break;
+        end;
+      end;
+      case QuestionDlg('Hier existiert schon ein Zug',
+          'An dieser Stelle existiert bereits ein Zug. Was soll getan werden?',
+          mtInformation, [30, 'Zug ersetzen', 31, 'Neue Variante', 32,
+          'Neue Hauptvariante', 33, 'Abbrechen'], '') of
+        30: CurrentGame.ReplaceMainLine(AMove);
+        31: CurrentGame.AddMoveAsSideLine(AMove);
+        32: CurrentGame.AddMoveAsNewMainLine(AMove);
+        33:
+        begin
+          AMove.Free;
+          Exit;
+        end;
+      end;
+    end;
+  end
+  else
+    AMove.Free;
+  Board1.CurrentPosition.Copy(CurrentGame.CurrentPosition);
+  NotationMemo1.SetTextFromGame(CurrentGame);
+  NotationMemo1.HighlightMove(CurrentGame.CurrentPlyNode.Data.Move);
 end;
 
 procedure TForm1.FormDestroy(Sender: TObject);
@@ -107,6 +183,27 @@ end;
 procedure TForm1.miSettingsClick(Sender: TObject);
 begin
   SettingsForm1.ShowModal;
+end;
+
+procedure TForm1.NotationMemo1ClickMove(Sender: TObject; AMove: TMove);
+begin
+  CurrentGame.GoToPositionAfterMove(AMove);
+  NotationMemo1.HighlightMove(AMove);
+  Board1.CurrentPosition.Copy(CurrentGame.CurrentPosition);
+  Board1.Invalidate;
+end;
+
+procedure TForm1.NotationMemo1Enter(Sender: TObject);
+begin
+  // never allow focus on NotationMemo1
+  Board1.SetFocus;
+end;
+
+procedure TForm1.NotationMemo1SelectionChange(Sender: TObject);
+begin
+  // Don't allow any selection
+  if NotationMemo1.SelLength > 0 then
+    NotationMemo1.SelLength := 0;
 end;
 
 end.
