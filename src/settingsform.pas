f562cd5d5dfa14ae0d29b8089a1ba63564c1d13d
jacade <j-c.dette@arcor.de>
2017-03-30 16:24:05 +0200

    Add saving of settings and a tab for engine config

--- src/settingsform.pas
+++ src/settingsform.pas
@@ -22,7 +22,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ComCtrls,
-  ExtCtrls, StdCtrls, Board, previewposition;
+  ExtCtrls, StdCtrls, Board, previewposition, UCI, UCIOptions, mSettings;
 
 type
 
@@ -31,6 +31,11 @@ type
   TSettingsForm = class(TForm)
     Board1: TBoard;
     Board2: TBoard;
+    btNewEngine: TButton;
+    btEditEngine: TButton;
+    btDeleteEngine: TButton;
+    bStandard: TButton;
+    bApply: TButton;
     cbBlack: TColorButton;
     cbWhite: TColorButton;
     CheckGroup1: TCheckGroup;
@@ -40,21 +45,33 @@ type
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
+    lEngines: TLabel;
+    lvEngines: TListView;
+    OpenDialog1: TOpenDialog;
     PageControl1: TPageControl;
     Panel1: TPanel;
     Panel2: TPanel;
+    tsEngines: TTabSheet;
     tsBoard: TTabSheet;
     TreeView1: TTreeView;
+    procedure btNewEngineClick(Sender: TObject);
+    procedure bApplyClick(Sender: TObject);
     procedure cbBackgroundColorChanged(Sender: TObject);
     procedure cbBlackColorChanged(Sender: TObject);
     procedure cbWhiteColorChanged(Sender: TObject);
     procedure CheckGroup1ItemClick(Sender: TObject; Index: integer);
     procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure FormShow(Sender: TObject);
     procedure TreeView1SelectionChanged(Sender: TObject);
   private
-    { private declarations }
+    EngineList: TUCIEngineSettingsList;
+    FSaveFile: string;
+    FSettings: TSettings;
+    procedure UpdateEngineListView;
   public
-    { public declarations }
+    property Settings: TSettings read FSettings write FSettings;
+    property SaveFile: string read FSaveFile write FSaveFile;
   end;
 
 var
@@ -71,6 +88,25 @@ begin
   PageControl1.TabIndex := TreeView1.Selected.Index;
 end;
 
+procedure TSettingsForm.UpdateEngineListView;
+var
+  UCIEngineSettings: TUCIEngineSettings;
+  Listitem: TListItem;
+begin
+  lvEngines.Clear;
+  for UCIEngineSettings in EngineList do
+  begin
+    ListItem := lvEngines.Items.Add;
+    Listitem.Caption := UCIEngineSettings.Name;
+    if UCIEngineSettings.Standard then
+      Listitem.SubItems.Add('Ja')
+    else
+      Listitem.SubItems.Add('Nein');
+    Listitem.SubItems.Add(DateTimeToStr(UCIEngineSettings.CreationDate));
+    Listitem.SubItems.Add(UCIEngineSettings.Authors);
+  end;
+end;
+
 procedure TSettingsForm.cbBlackColorChanged(Sender: TObject);
 begin
   Board1.BlackSquareColor := cbBlack.ButtonColor;
@@ -80,6 +116,47 @@ end;
 procedure TSettingsForm.cbBackgroundColorChanged(Sender: TObject);
 begin
   Board1.Border.Background := cbBackground.ButtonColor;
+end;
+
+procedure TSettingsForm.btNewEngineClick(Sender: TObject);
+var
+  NewEngine: TUCIEngine;
+  UCIEngineSettings: TUCIEngineSettings;
+begin
+  if OpenDialog1.Execute then
+  begin
+    NewEngine := TUCIEngine.Create(nil);
+    NewEngine.ProcessName := OpenDialog1.FileName;
+    NewEngine.Init;
+    NewEngine.Quit;
+    UCIEngineSettings := TUCIEngineSettings.Create;
+    UCIEngineSettings.Authors := NewEngine.Author;
+    UCIEngineSettings.CreationDate := Now;
+    UCIEngineSettings.ExecuteFile := OpenDialog1.FileName;
+    UCIEngineSettings.Name := NewEngine.EngineName;
+    UCIEngineSettings.Standard := False;
+    EngineList.Add(UCIEngineSettings);
+    UpdateEngineListView;
+    NewEngine.Free;
+  end;
+end;
+
+procedure TSettingsForm.bApplyClick(Sender: TObject);
+begin
+  // Board Colors
+  Settings.BoardSettings.BackgroundColor := cbBackground.ButtonColor;
+  Settings.BoardSettings.BlackSquareColor := cbBlack.ButtonColor;
+  Settings.BoardSettings.WhiteSquareColor := cbWhite.ButtonColor;
+  // Border sides
+  Settings.BoardSettings.ShowBorderLeft := CheckGroup1.Checked[0];
+  Settings.BoardSettings.ShowBorderRight := CheckGroup1.Checked[1];
+  Settings.BoardSettings.ShowBorderTop := CheckGroup1.Checked[2];
+  Settings.BoardSettings.ShowBorderBottom := CheckGroup1.Checked[3];
+  // Engines
+  Settings.UCIEngineSettingsList.Free;
+  Settings.UCIEngineSettingsList := EngineList;
+
+  Settings.SaveToFile(SaveFile);
 end;
 
 procedure TSettingsForm.cbWhiteColorChanged(Sender: TObject);
@@ -107,6 +184,43 @@ begin
   TreeView1.Selected := TreeView1.Items[0];
   for i := 0 to 3 do
     CheckGroup1.Checked[i] := True;
+  EngineList := TUCIEngineSettingsList.Create;
+end;
+
+procedure TSettingsForm.FormDestroy(Sender: TObject);
+begin
+  if Self.ModalResult <> mrOk then
+    EngineList.Free;
+end;
+
+procedure TSettingsForm.FormShow(Sender: TObject);
+var
+  i: integer;
+  UCIEngineSettings, Temp: TUCIEngineSettings;
+begin
+  // Color buttons
+  cbBackground.ButtonColor := Settings.BoardSettings.BackgroundColor;
+  cbBlack.ButtonColor := Settings.BoardSettings.BlackSquareColor;
+  cbWhite.ButtonColor := Settings.BoardSettings.WhiteSquareColor;
+  // CheckGroup1
+  CheckGroup1.Checked[0] := Settings.BoardSettings.ShowBorderLeft;
+  CheckGroup1.Checked[1] := Settings.BoardSettings.ShowBorderRight;
+  CheckGroup1.Checked[2] := Settings.BoardSettings.ShowBorderTop;
+  CheckGroup1.Checked[3] := Settings.BoardSettings.ShowBorderBottom;
+  for i := 0 to 3 do
+    CheckGroup1ItemClick(Self, i);
+  // Engines
+  for UCIEngineSettings in Settings.UCIEngineSettingsList do
+  begin
+    Temp := TUCIEngineSettings.Create;
+    Temp.Authors := UCIEngineSettings.Authors;
+    Temp.CreationDate := UCIEngineSettings.CreationDate;
+    Temp.ExecuteFile := UCIEngineSettings.ExecuteFile;
+    Temp.Name := UCIEngineSettings.Name;
+    Temp.Standard := UCIEngineSettings.Standard;
+    EngineList.Add(Temp);
+  end;
+  UpdateEngineListView;
 end;
 
 end.
